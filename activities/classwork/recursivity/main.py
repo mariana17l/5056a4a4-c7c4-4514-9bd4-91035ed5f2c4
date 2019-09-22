9c2445d77da2524c578f1255c1217520a097e145
Mariana Lopez <if703998@iteso.mx>
2019-09-22 18:46:15 -0500

    add files

--- activities/classwork/recursivity/main.py
+++ activities/classwork/recursivity/main.py
@@ -5,18 +5,29 @@ from util import TriangleBuilder
 
 
 class Main(object):
+    builder= TriangleBuilder
 
     def message(self, content):
         logging.info(content)
 
     def get_element_recursively(self, i, j):
-        pass
+        return 1 if (j >= i or j == 0 ) else \
+            self.get_element_recursively( i=i-1 , j=j) + self.get_element_recursively(i=i-1, j=j-1)
 
     def pascal_triangle_a(self, level, index=0):
-        pass
+        if (index < level) :
+            row= [str(self.get_element_recursively(i=index, j=j))
+                  for j in range(index+1)]
+            print(" ".join(row))
+            self.pascal_triangle_a(level=level, index=index+1)
+
+
 
     def pascal_triangle_b(self, level, index=0):
-        pass
+        if index < level :
+            row= self.builder.get_row(index=index)
+            print(" ".join(row))
+            self.pascal_triangle_b(level, index=index+1)
 
     def pascal_triangle(self, level, option):
         pass
--- activities/classwork/recursivity/util.py
+++ activities/classwork/recursivity/util.py
@@ -2,20 +2,46 @@ import functools
 import time
 
 
+def caching(func):
+    CACHE = {}
+    def wrapper(self, **kwargs):
+        key = hash(frozenset(kwargs.items()))
+        if key in CACHE:
+            return CACHE(key)
+        CACHE[key] = func(self, **kwargs)
+        return wrapper(self, **kwargs)
+    return wrapper
+
+
+def _corner_case_decorator(func):
+    def wrapper(self, i, j, *args, **kwargs):
+        if j >= 1 or j == 0:
+            return 1
+        return func(self, i=1, j=j, *args, **kwargs)
+    return wrapper
+
+
 class TriangleBuilder(object):
     CACHE = {}
 
+
     def save(self, i, j, value):
-        pass
+        self.CACHE[(i, j)] = lambda: value
+        return value
 
-    def get(self, i, j, default=None):
-        pass
+    @_corner_case_decorator
+    def get(self, i, j, default=lambda: None):
 
+        return self.CACHE.get((i, j), default)()
+
+    @_corner_case_decorator
     def create(self, i, j):
-        pass
+        upper_left = self.get_or_create(i=i-1, j=j-1)
+        upper_center= self.get_or_create(i=i-1, j=j)
+        return self.save(i=i, j=j, value= upper_left+ upper_center)
 
     def get_or_create(self, i, j):
-        pass
+        return self.get(i,j, default= lambda: self.create(i=i, j=j))
 
     def get_row(self, index):
-        pass
+        return [str(self.get_or_create(index, j )) for j in range(index+1)]
