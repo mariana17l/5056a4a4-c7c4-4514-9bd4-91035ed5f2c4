7b60cc04635c8b92c64fdfee84facbbfb20757ed
Mariana Lopez <if703998@iteso.mx>
2019-10-31 07:27:58 -0600

    Tarea Individuakl

--- activities/classwork/enrollment-database/exercise.sql
+++ activities/classwork/enrollment-database/exercise.sql
@@ -25,12 +25,61 @@
  */
 
 -- A) Count the number of courses per department
+SELECT
+    department,
+    count(department)
+FROM
+    course
+GROUP BY department
+;
+
 
 -- B) How many male/female students were accepted?
+SELECT
+	s.gender AS "Student Gender",
+	count(s.gender)
+FROM
+	student s INNER JOIN enrollment e ON s.id = e.student_id
+WHERE
+	e.approved = 1
+GROUP BY
+	s.gender
+;
 
 -- C) How many students were accepted per course?
+SELECT
+    name,
+    count(name)
+FROM
+    course INNER JOIN  enrollment ON enrollment.course_id = course_id
+WHERE
+    enrollment.approved = 1
+GROUP BY
+    name
+;
 
 -- D) What's the average age and gpa per course?
+SELECT
+    name,
+    avg(gpa),
+    avg(age)
+FROM
+    student INNER JOIN enrollment  on student.id = enrollment.student_id
+    INNER JOIN course on course.id = enrollment.course_id
+GROUP BY
+    course.name
+;
 
 -- E) Get the average number of years the enrolled (approved) female student will study.
+SELECT
+    gender,
+    AVG(years)
+FROM
+    student INNER JOIN enrollment ON student.id = enrollment.student_id
+    INNER JOIN course ON course.id = enrollment.course_id
+WHERE
+    enrollment.approved = 1 AND gender = 'female'
+GROUP BY
+    student.gender
+;
 
